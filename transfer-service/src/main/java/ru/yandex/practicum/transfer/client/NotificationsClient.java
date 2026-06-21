85dde6492f0e1337fec3d576232352a1e23d64cf
rsidash <roman.sidash@kaspi.kz>
2026-06-21 19:44:02 +0500

    refactor(transfer): replace REST NotificationsClient with Kafka producer

--- transfer-service/src/main/java/ru/yandex/practicum/transfer/client/NotificationsClient.java
+++ transfer-service/src/main/java/ru/yandex/practicum/transfer/client/NotificationsClient.java
@@ -1,36 +1,23 @@
 package ru.yandex.practicum.transfer.client;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClient;
-import ru.yandex.practicum.transfer.config.NotificationsServiceProperties;
-import ru.yandex.practicum.transfer.config.OAuth2TokenService;
 
 import java.util.Map;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class NotificationsClient {
 
-    private final RestClient restClient;
-    private final OAuth2TokenService tokenService;
-
-    public NotificationsClient(NotificationsServiceProperties properties,
-                               RestClient.Builder builder,
-                               OAuth2TokenService tokenService) {
-        this.restClient = builder.baseUrl(properties.getUrl()).build();
-        this.tokenService = tokenService;
-    }
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public void notifyTransfer(String fromLogin, String toLogin, int value) {
         try {
-            String token = tokenService.getToken("notifications");
-            restClient.post()
-                    .uri("/transfer")
-                    .header("Authorization", "Bearer " + token)
-                    .body(Map.of("fromLogin", fromLogin, "toLogin", toLogin, "value", value))
-                    .retrieve()
-                    .toBodilessEntity();
+            kafkaTemplate.send("notifications.transfer",
+                    fromLogin, Map.of("fromLogin", fromLogin, "toLogin", toLogin, "value", value));
         } catch (Exception e) {
             log.warn("Failed to send transfer notification: {}", e.getMessage(), e);
         }
